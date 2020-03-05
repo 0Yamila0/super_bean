a8951ab80cad9397b5d469bebc1d32dff5bd1f46
dylanjones22 <46570384+dylanjones22@users.noreply.github.com>
2020-03-05 09:46:57 -0600

    vsdfxvxdfvfxdv

--- index.php
+++ index.php
@@ -15,15 +15,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <p> Super Bean </p>
-
-        
-       
-  
       </ul>
     </div>
   </nav>
  
-
 
   <div id="first"> <div id="blob"> </div></div>
   <div id="second">  <div id="blob2"> </div></div>
@@ -31,11 +26,11 @@
   <div id="fourth"> <div id="blob4"> </div></div>
   <div id="fifth"> <div id="blob5"> </div></div>
 
-  
-    <div class="space"></div>
+
       <div id="fifth">   <a onclick="toTop('first'); return false;" href= "#first">Go to Top</a>
 
         <a class="navbar-brand" href="index.php"> </a>
+
         <a class="nav-link" href="index.php"> Home <span class="sr-only">(current)</span></a>
    
 
@@ -46,7 +41,7 @@
   
 
         <a class="nav-link" href="login.php"> Account</a>
-
+      </div>
       </footer>
 
   
--- step1.php
+++ step1.php
@@ -14,39 +14,53 @@
     </head>
 <body>
   <nav class="navbar navbar-expand-lg">
+
     <a class="navbar-brand" href="index.html"> </a>
+
   
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
           <a class="nav-link" href="index.html"> Home <span class="sr-only">(current)</span></a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="topsbean.html">Top Beans</a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="step1.html"> Build A Bean</a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="login.html"> Account</a>
         </li>
+
       </ul>
     </div>
   </nav>
+
+
     <h1 class=" section text-center"> BUILD-A-BEAN</h1>
+
     <div class="container-fluid">
+
     <div class="row no-gutters full-width-row">
       <div class="col">
         
         <p class="headings text-center"> Pick Your Color!</p>
+
         <div class="col-6 remove-all-margin">
+
         <div class="card bg-dark text-black"> <a class="nextstep" href="step2.html">
             <img src="img/color1.png" class="card-img">
             <div class="card-img-overlay">
               <h5 class="card-title"> Yellow </h5>
+
               <p class="card-text"> ksdnfklnsdfjklsdnfljknsd
                 d;kfnlkdnvdjklfvnlkjdfnvkldsnvklddnvldkf
               </p>
+
               <p> dkjnfkdjcvnljkdnvlkdfnvklnfv
                 dslkcfnsdlnjhdsnjfcndc
               </p>
@@ -54,15 +68,23 @@
           </a>
         </div>
       </div>
+
+
           <br>
           <div class="col-6 remove-all-margin">
+
           <div class="card bg-dark text-black"> <a class="nextstep" href="step2.html">
+
             <img src="img/color1.png" class="card-img">
+
             <div class="card-img-overlay">
+
               <h5 class="card-title"> Yellow </h5>
+
               <p class="card-text"> ksdnfklnsdfjklsdnfljknsd
                 d;kfnlkdnvdjklfvnlkjdfnvkldsnvklddnvldkf
               </p>
+
               <p> dkjnfkdjcvnljkdnvlkdfnvklnfv
                 dslkcfnsdlnjhdsnjfcndc
               </p>
@@ -70,15 +92,22 @@
           </a>
           </div>
           </div>
+
           <br> 
           <div class="col-6">
+
           <div class="card bg-dark text-black"> <a class="nextstep" href="step2.html">
+
             <img src="img/color1.png" class="card-img">
+
             <div class="card-img-overlay">
+
               <h5 class="card-title"> Yellow </h5>
+
               <p class="card-text"> ksdnfklnsdfjklsdnfljknsd
                 d;kfnlkdnvdjklfvnlkjdfnvkldsnvklddnvldkf
               </p>
+
               <p> dkjnfkdjcvnljkdnvlkdfnvklnfv
                 dslkcfnsdlnjhdsnjfcndc
               </p>
@@ -86,30 +115,46 @@
           </a>
           </div> 
           </div>
+
           <br>
           <div class="col-6">
+
           <div class="card bg-dark text-black"> <a class="nextstep" href="step2.html">
+
             <img src="img/color1.png" class="card-img">
+
             <div class="card-img-overlay">
+
               <h5 class="card-title"> Yellow </h5>
+
               <p class="card-text"> ksdnfklnsdfjklsdnfljknsd
                 d;kfnlkdnvdjklfvnlkjdfnvkldsnvklddnvldkf
               </p>
+
               <p> dkjnfkdjcvnljkdnvlkdfnvklnfv
                 dslkcfnsdlnjhdsnjfcndc
               </p>
+
             </div>
           </a>
           </div>  
           </div>
     </div>
+
     <div class="col text-center">
+
         <p class="headings"> Pick Your Step!</p>
+
         <ul class="list-group">
+
           <li class="list-group-item"><a href="step1.html"> Step 1</a></li>
+
           <li class="list-group-item"><a href="step2.html"> Step 2</a></li>
+
           <li class="list-group-item"><a href="step3.html"> Step 3</a></li>
+
           <li class="list-group-item"> <a href="step4.html"> Step 4</a></li>
+          
         </ul>
     </div>
     </div>
