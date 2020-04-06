0ab1d963794d895cf36ca813991d2abfaf81c6a7
Amber Owens <42219204+amber-owens@users.noreply.github.com>
2020-04-06 17:14:13 -0500

    removed instagram logo

--- INC/footer.php
+++ INC/footer.php
@@ -6,11 +6,6 @@
 	<strong>Contact Info</strong>
 	<p>(323) 319-6060<br>daybean@jmcss.org</p>
 
-<i class="fa fa-instagram" style="font-size:24px">Instagram</i>
-
-
-
-
 <hr class="socket">
 &copy; DAY Time
 	</div>
